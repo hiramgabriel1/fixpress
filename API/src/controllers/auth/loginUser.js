6f77ab27dd9cc0c3577dab4345df0c8da5d2b0ad
itsrustycode <82051017+itsrusty@users.noreply.github.com>
2023-04-30 16:52:50 -0700

    Delete API/src directory

--- API/src/controllers/auth/loginUser.js
+++
@@ -1,3 +0,0 @@
-export const loginUser = async (req, res) => {
-    res.send("test")
-}
--- API/src/routes/loginUser.routes.js
+++
@@ -1,9 +0,0 @@
-import { Router } from "express";
-import { loginUser } from "../controllers/auth/loginUser.js";
-
-// instance router
-const router = Router();
-
-router.get("/", loginUser)
-
-export default router
